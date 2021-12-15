e7b0f37bdcb63a54b1f2ad07cc4e17b9fb68e3eb
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2021-12-14 22:25:58 -0300

    Adicionado filtro por cidade

--- frontend/src/app/core/weather/weather.service.ts
+++ frontend/src/app/core/weather/weather.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { City } from './city';
 import { Weather } from './weather';
 
 const BASE_URL = "https://apiadvisor.climatempo.com.br/api/v1";
@@ -17,5 +18,10 @@ export class WeatherService {
   public getWeather(cityCode: string): Observable<Weather> {
     return this.http
       .get<Weather>(BASE_URL + "/weather/locale/" + cityCode + "/current?token=" + TOKEN );
+  }
+ 
+  public getCity(cityName: string): Observable<City[]> {
+    return this.http
+      .get<City[]>(BASE_URL + '/locale/city?name=' + cityName + '&token=' + TOKEN );
   }
 }
--- frontend/src/app/dashboard/weather/weather.component.ts
+++ frontend/src/app/dashboard/weather/weather.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { City } from 'src/app/core/weather/city';
 import { Weather } from 'src/app/core/weather/weather';
 import { WeatherService } from 'src/app/core/weather/weather.service';
 
@@ -8,16 +9,44 @@ import { WeatherService } from 'src/app/core/weather/weather.service';
 })
 export class WeatherComponent implements OnInit {
 
+  @ViewChild('city') cityElement: ElementRef;
+
   cidade: string = "5425";
   data: Weather;
 
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService, cityElement: ElementRef) {
+    this.cityElement = cityElement;
+  }
 
   ngOnInit(): void {
+    this.refreshData();
+  }
+
+  refreshData(): void {
     this.weatherService.getWeather(this.cidade)
-    .subscribe(
-      data => this.data = data ,
-      err => console.log("Erro!"));
+      .subscribe(
+        data => this.data = data,
+        err => console.log("Erro!"));
+  }
+
+  searchCity() {
+    const cityName = this.cityElement.nativeElement.value;
+
+    this.weatherService.getCity(cityName)
+      .subscribe(
+        cityList => {
+          if (!cityList) return
+
+          cityList.forEach(city => {
+            if (city.name == cityName) {
+              this.cidade = (city.id as unknown) as string;
+            }
+          });
+
+          this.refreshData();
+        },
+        err => console.log("Erro!")
+      );
   }
 
 }
